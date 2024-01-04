dbd2706f000b8df474452131b84ca1aed0d3963f
bchollid <brandonholliday1@gmail.com>
2024-01-04 06:27:50 -0700

    Updated with correct list removal

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace CollectionsMasterConsoleUI
@@ -8,61 +9,61 @@ namespace CollectionsMasterConsoleUI
         static void Main(string[] args)
         {
 
-            var fiftyArray = new int[50];
+            //var fiftyArray = new int[50];
 
-            Populater(fiftyArray);
+            //Populater(fiftyArray);
 
 
-            Console.WriteLine("All Numbers Original");
-            NumberPrinter(fiftyArray);
-            Console.WriteLine("-------------------");
+            //Console.WriteLine("All Numbers Original");
+            //NumberPrinter(fiftyArray);
+            //Console.WriteLine("-------------------");
 
-            Console.WriteLine("All Numbers Reversed:");
+            //Console.WriteLine("All Numbers Reversed:");
 
-            Array.Reverse(fiftyArray);
+            //Array.Reverse(fiftyArray);
 
-            NumberPrinter(fiftyArray);
+            //NumberPrinter(fiftyArray);
 
-            Console.WriteLine("---------REVERSE CUSTOM------------");
+            //Console.WriteLine("---------REVERSE CUSTOM------------");
 
-            ReverseArray(fiftyArray);
-            NumberPrinter(fiftyArray);
+            //ReverseArray(fiftyArray);
+            //NumberPrinter(fiftyArray);
 
-            Console.WriteLine("-------------------");
+            //Console.WriteLine("-------------------");
 
-            Console.WriteLine("Multiple of three = 0: ");
+            //Console.WriteLine("Multiple of three = 0: ");
 
-            ThreeKiller(fiftyArray);
-            NumberPrinter(fiftyArray);
+            //ThreeKiller(fiftyArray);
+            //NumberPrinter(fiftyArray);
 
-            Console.WriteLine("-------------------");
+            //Console.WriteLine("-------------------");
 
-            Console.WriteLine("Sorted numbers:");
-            Array.Sort(fiftyArray);
-            NumberPrinter(fiftyArray);
+            //Console.WriteLine("Sorted numbers:");
+            //Array.Sort(fiftyArray);
+            //NumberPrinter(fiftyArray);
 
-            Console.WriteLine("\n************End Arrays*************** \n");
-            Console.WriteLine("************Start Lists**************");
+            //Console.WriteLine("\n************End Arrays*************** \n");
+            //Console.WriteLine("************Start Lists**************");
 
             var myList = new List<int>();
-            Console.WriteLine(myList.Capacity);
+            //Console.WriteLine(myList.Capacity);
             Populater(myList);
-            Console.WriteLine(myList.Capacity);
+            //Console.WriteLine(myList.Capacity);
 
-            Console.WriteLine("---------------------");
+            //Console.WriteLine("---------------------");
 
-            Console.WriteLine("What number will you search for in the number list?");
-            var userNumber = 0;
-            if (Int32.TryParse(Console.ReadLine(), out userNumber))
-            {
-                NumberChecker(myList, userNumber);
-            }
-            else
-            {
-                Console.WriteLine("Please enter a valid number between 0 and 50.");
-            }
-            
-            Console.WriteLine("-------------------");
+            //Console.WriteLine("What number will you search for in the number list?");
+            //var userNumber = 0;
+            //if (Int32.TryParse(Console.ReadLine(), out userNumber))
+            //{
+            //    NumberChecker(myList, userNumber);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Please enter a valid number between 0 and 50.");
+            //}
+
+            //Console.WriteLine("-------------------");
 
             Console.WriteLine("All Numbers:");
             NumberPrinter(myList);
@@ -73,17 +74,17 @@ namespace CollectionsMasterConsoleUI
             OddKiller(myList);
             NumberPrinter(myList);
 
-            Console.WriteLine("------------------");
+            //Console.WriteLine("------------------");
 
-            Console.WriteLine("Sorted Evens!!");
+            //Console.WriteLine("Sorted Evens!!");
 
-            myList.Sort();
-            NumberPrinter(myList);
+            //myList.Sort();
+            //NumberPrinter(myList);
            
-            Console.WriteLine("------------------");
+            //Console.WriteLine("------------------");
 
-            var listIntoArray = myList.ToArray();
-            myList.Clear();
+            //var listIntoArray = myList.ToArray();
+            //myList.Clear();
             
         }
 
@@ -100,13 +101,7 @@ namespace CollectionsMasterConsoleUI
 
         private static void OddKiller(List<int> numberList)
         {
-            for(var i = 0; i<numberList.Count; i++)
-            {
-                if (numberList[i] % 2 == 0)
-                {
-                    numberList.Remove(numberList[i]);
-                }
-            }
+            numberList.RemoveAll(i => i % 2 != 0);
         }
 
         private static void NumberChecker(List<int> numberList, int searchNumber)
